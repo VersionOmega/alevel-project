b25279652184344f6daed2a8f67f2ed71a889fd2
VersionOmega <danielroberts43@outlook.com>
2022-09-03 18:18:48 +0100

    Add files via upload

---
+++ maps/64TilesheetDungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="64TilesheetDungeon" tilewidth="64" tileheight="64" spacing="4" tilecount="522" columns="29">
+ <image source="Sheets/64TilesheetDungeon.png" width="1968" height="1220"/>
+</tileset>
